0606b510417a4ecbd7e48d7bd12c5eb2f56e7c97
Sebastian <sebastian97@gmail.com>
2024-12-15 18:49:19 -0800

    Fix swift sneak enchanting near warden

--- src/main/java/com/jsorrell/carpetskyadditions/mixin/EnchantmentHelperMixin.java
+++ src/main/java/com/jsorrell/carpetskyadditions/mixin/EnchantmentHelperMixin.java
@@ -3,13 +3,20 @@ package com.jsorrell.carpetskyadditions.mixin;
 import static com.jsorrell.carpetskyadditions.helpers.SkyAdditionsEnchantmentHelper.SWIFT_SNEAK_ENCHANTABLE_TAG;
 
 import com.google.common.collect.Lists;
+import com.jsorrell.carpetskyadditions.SkyAdditionsExtension;
 import com.jsorrell.carpetskyadditions.helpers.SkyAdditionsEnchantmentHelper;
 
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
+import com.jsorrell.carpetskyadditions.util.SkyAdditionsDataComponents;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -19,32 +26,38 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraft.world.item.enchantment.Enchantments;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(EnchantmentHelper.class)
-public class EnchantmentHelperMixin {
+public abstract class EnchantmentHelperMixin {
+
+
     @Inject(
         method = "getAvailableEnchantmentResults",
-        at = @At(
+        at =
+        @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"
-        ),
-        locals = LocalCapture.CAPTURE_FAILSOFT
-    )
-    private static void forceAllowSwiftSneak(int i, ItemStack itemStack, Stream<Holder<Enchantment>> stream, CallbackInfoReturnable<List<EnchantmentInstance>> cir) {
+            target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"),
+        locals = LocalCapture.CAPTURE_FAILSOFT)
+    private static void forceAllowSwiftSneak(
+        int i, ItemStack stack, Stream<Holder<Enchantment>> stream,
+        CallbackInfoReturnable<List<EnchantmentInstance>> cir, @Local List<EnchantmentInstance> enchantmentList) {
+        Holder.Reference<Enchantment> swiftSneak = SkyAdditionsExtension.minecraftServer.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).get(Enchantments.SWIFT_SNEAK).orElseThrow();
+        if (Boolean.TRUE.equals(stack.get(SkyAdditionsDataComponents.SWIFT_SNEAK_ENCHANTABLE_COMPONENT))) {
 
-        if (itemStack.getTags().findAny().isPresent() && itemStack.getTags().anyMatch(tag -> tag.equals(SWIFT_SNEAK_ENCHANTABLE_TAG))) {
-            if ( /*Enchantments.SWIFT_SNEAK.canEnchant(itemStack.getItem().getDefaultInstance()) ||itemStack.is(Items.BOOK)*/ true) {
+            if (   swiftSneak.value().canEnchant(stack) || stack.is(Items.BOOK)) {
                 for (int level = 3; 1 <= level; --level) {
-                    if (SkyAdditionsEnchantmentHelper.getSwiftSneakMinCost(level) <= i
-                            && i <= SkyAdditionsEnchantmentHelper.getSwiftSneakMaxCost(level)) {
-                        List<EnchantmentInstance> list = Lists.newArrayList();
-                        list.add(new EnchantmentInstance((Holder<Enchantment>) Enchantments.SWIFT_SNEAK, level));
-                        break;
-                    }
+                    /*if (SkyAdditionsEnchantmentHelper.getSwiftSneakMinCost(level) <= i
+                        && i <= SkyAdditionsEnchantmentHelper.getSwiftSneakMaxCost(level)) {*/
+                        enchantmentList.add(new EnchantmentInstance(
+                            swiftSneak,
+                            level));
+                        //break;
+                    //}
                 }
             }
         }
