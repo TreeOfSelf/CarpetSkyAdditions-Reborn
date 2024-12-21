fd236fa2064fd6a65b5ff53bee3f894a09c68fe7
Sebastian <sebastian97@gmail.com>
2024-12-21 14:04:11 -0800

    Convert blaze to breeze when transferring from nether

---
+++ src/main/java/com/jsorrell/carpetskyadditions/mixin/BlazeToBreezeMixin.java
@@ -0,0 +1,27 @@
+package com.jsorrell.carpetskyadditions.mixin;
+
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.monster.Blaze;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Entity.class)
+public abstract class BlazeToBreezeMixin {
+
+    @Redirect(
+        method = "teleportCrossDimension",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/EntityType;create(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/EntitySpawnReason;)Lnet/minecraft/world/entity/Entity;")
+    )
+    private Entity redirectEntityCreation(EntityType<?> entityType, Level level, EntitySpawnReason reason) {
+        if ((Object) this instanceof Blaze) {
+            Blaze blaze = (Blaze)(Object) this;
+            if (blaze.level().dimension() == Level.NETHER && !blaze.hasCustomName()) {
+                return EntityType.BREEZE.create(level, reason);
+            }
+        }
+
+        return entityType.create(level, reason);
+    }
+}
