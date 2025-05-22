d4fd201a3cb7b6d204239184e6568a31f6895b71
Sebastian <sebastian97@gmail.com>
2025-05-22 14:01:29 -0700

    Update WanderingTraderHelper.java

--- src/main/java/com/jsorrell/carpetskyadditions/helpers/WanderingTraderHelper.java
+++ src/main/java/com/jsorrell/carpetskyadditions/helpers/WanderingTraderHelper.java
@@ -6,35 +6,38 @@ import java.util.Arrays;
 import java.util.List;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.Items;
-import org.apache.commons.lang3.tuple.Pair; 
+import org.apache.commons.lang3.tuple.Pair;
 
 public class WanderingTraderHelper {
     public static List<Pair<VillagerTrades.ItemListing[], Integer>> getTrades() {
-        Pair<VillagerTrades.ItemListing[], Integer> tier1VanillaPair = VillagerTrades.WANDERING_TRADER_TRADES.get(0);
-        Pair<VillagerTrades.ItemListing[], Integer> tier2VanillaPair = VillagerTrades.WANDERING_TRADER_TRADES.get(1);
-
-        List<VillagerTrades.ItemListing> tier1List = new ArrayList<>(Arrays.asList(tier1VanillaPair.getLeft()));
-        List<VillagerTrades.ItemListing> tier2List = new ArrayList<>(Arrays.asList(tier2VanillaPair.getLeft()));
-
-        if (SkyAdditionsSettings.tallFlowersFromWanderingTrader) {
-            tier1List.add(new VillagerTrades.ItemsForEmeralds(Items.SUNFLOWER, 1, 1, 12, 1));
-            tier1List.add(new VillagerTrades.ItemsForEmeralds(Items.LILAC, 1, 1, 12, 1));
-            tier1List.add(new VillagerTrades.ItemsForEmeralds(Items.ROSE_BUSH, 1, 1, 12, 1));
-            tier1List.add(new VillagerTrades.ItemsForEmeralds(Items.PEONY, 1, 1, 12, 1));
-        }
-        tier1List.add(new VillagerTrades.ItemsForEmeralds(Items.PINK_PETALS, 1, 16, 12, 1));
-
-        if (SkyAdditionsSettings.lavaFromWanderingTrader) {
-            tier2List.add(new VillagerTrades.ItemsAndEmeraldsToItems(Items.BUCKET, 1, 16, Items.LAVA_BUCKET, 1, 1, 1, 1));
-        }
-
-        VillagerTrades.ItemListing[] tier1Array = tier1List.toArray(new VillagerTrades.ItemListing[0]);
-        VillagerTrades.ItemListing[] tier2Array = tier2List.toArray(new VillagerTrades.ItemListing[0]);
-
+        List<Pair<VillagerTrades.ItemListing[], Integer>> originalTradeGroups = VillagerTrades.WANDERING_TRADER_TRADES;
         List<Pair<VillagerTrades.ItemListing[], Integer>> newTrades = new ArrayList<>();
-        newTrades.add(Pair.of(tier1Array, tier1VanillaPair.getRight())); 
-        newTrades.add(Pair.of(tier2Array, tier2VanillaPair.getRight())); 
-        
+
+        for (int i = 0; i < originalTradeGroups.size(); i++) {
+            Pair<VillagerTrades.ItemListing[], Integer> currentVanillaPair = originalTradeGroups.get(i);
+            List<VillagerTrades.ItemListing> currentGroupTradeList = new ArrayList<>(Arrays.asList(currentVanillaPair.getLeft()));
+            Integer tradesToPickFromGroup = currentVanillaPair.getRight();
+
+            if (i == 0) {
+                if (SkyAdditionsSettings.tallFlowersFromWanderingTrader) {
+                    currentGroupTradeList.add(new VillagerTrades.ItemsForEmeralds(Items.SUNFLOWER, 1, 1, 12, 1));
+                    currentGroupTradeList.add(new VillagerTrades.ItemsForEmeralds(Items.LILAC, 1, 1, 12, 1));
+                    currentGroupTradeList.add(new VillagerTrades.ItemsForEmeralds(Items.ROSE_BUSH, 1, 1, 12, 1));
+                    currentGroupTradeList.add(new VillagerTrades.ItemsForEmeralds(Items.PEONY, 1, 1, 12, 1));
+                }
+
+                currentGroupTradeList.add(new VillagerTrades.ItemsForEmeralds(Items.PINK_PETALS, 1, 16, 12, 1));
+            } else if (i == 1) {
+                if (SkyAdditionsSettings.lavaFromWanderingTrader) {
+                    currentGroupTradeList.add(new VillagerTrades.ItemsAndEmeraldsToItems(Items.BUCKET, 1, 16, Items.LAVA_BUCKET, 1, 1, 1, 1));
+                }
+            }
+
+
+            VillagerTrades.ItemListing[] currentGroupTradeArray = currentGroupTradeList.toArray(new VillagerTrades.ItemListing[0]);
+            newTrades.add(Pair.of(currentGroupTradeArray, tradesToPickFromGroup));
+        }
+
         return newTrades;
     }
 }
