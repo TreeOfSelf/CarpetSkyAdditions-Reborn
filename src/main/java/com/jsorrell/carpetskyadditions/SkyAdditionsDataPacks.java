29216eca81d32e945391584b0d904daaf9931d87
Sebastian <sebastian97@gmail.com>
2025-08-09 20:47:43 -0700

    Remove unused packs

--- src/main/java/com/jsorrell/carpetskyadditions/SkyAdditionsDataPacks.java
+++ src/main/java/com/jsorrell/carpetskyadditions/SkyAdditionsDataPacks.java
@@ -10,9 +10,7 @@ public class SkyAdditionsDataPacks {
     public static final SkyAdditionsResourceLocation SKYBLOCK = new SkyAdditionsResourceLocation("skyblock");
     public static final SkyAdditionsResourceLocation SKYBLOCK_MMH = new SkyAdditionsResourceLocation("skyblock_mmh");
     public static final SkyAdditionsResourceLocation SKYBLOCK_ACACIA = new SkyAdditionsResourceLocation("skyblock_acacia");
-    public static final SkyAdditionsResourceLocation SKYBLOCK_ACACIA_MMH = new SkyAdditionsResourceLocation("skyblock_acacia_mmh");
     public static final SkyAdditionsResourceLocation SKYBLOCK_NO_TREE = new SkyAdditionsResourceLocation("skyblock_no_tree");
-    public static final SkyAdditionsResourceLocation SKYBLOCK_NO_TREE_MMH = new SkyAdditionsResourceLocation("skyblock_no_tree_mmh");
 
     public static void register() {
         // Add the embedded datapacks as an option on the create world screen
@@ -41,14 +39,6 @@ public class SkyAdditionsDataPacks {
         }
 
         if (!ResourceManagerHelper.registerBuiltinResourcePack(
-                new SkyAdditionsResourceLocation("skyblock_acacia_mmh").getResourceLocation(),
-                SkyAdditionsExtension.MOD_CONTAINER,
-                Component.translatable("datapack.carpetskyadditions.acacia_mmh"),
-                ResourcePackActivationType.NORMAL)) {
-            SkyAdditionsSettings.LOG.warn("Could not register built-in datapack \"skyblock_acacia_mmh\".");
-        }
-
-        if (!ResourceManagerHelper.registerBuiltinResourcePack(
                 new SkyAdditionsResourceLocation("skyblock_no_tree").getResourceLocation(),
                 SkyAdditionsExtension.MOD_CONTAINER,
                 Component.translatable("datapack.carpetskyadditions.no_tree"),
@@ -56,13 +46,6 @@ public class SkyAdditionsDataPacks {
             SkyAdditionsSettings.LOG.warn("Could not register built-in datapack \"skyblock_no_tree\".");
         }
 
-        if (!ResourceManagerHelper.registerBuiltinResourcePack(
-                new SkyAdditionsResourceLocation("skyblock_no_tree_mmh").getResourceLocation(),
-                SkyAdditionsExtension.MOD_CONTAINER,
-                Component.translatable("datapack.carpetskyadditions.no_tree_mmh"),
-                ResourcePackActivationType.NORMAL)) {
-            SkyAdditionsSettings.LOG.warn("Could not register built-in datapack \"skyblock_no_tree_mmh\".");
-        }
 
     }
 }
